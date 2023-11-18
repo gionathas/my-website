e39927122d573fb0ad03e87b9764d6098f8a8208
gionny96 <gionathas@gmail.com>
2023-11-18 22:44:53 +0100

    feat: add blog page

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,17 +2,15 @@ import Link from 'components/Link'
 import routes from 'config/routes'
 import useWindowScroll from 'lib/hooks/useWindowScroll'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
-import { GiHamburgerMenu } from 'react-icons/gi'
 
 const navigationLinks: { title: string; href: string }[] = [
-  // {
-  //   title: 'Bio',
-  //   href: routes.bio,
-  // },
   {
     title: 'Home',
     href: routes.home,
+  },
+  {
+    title: 'Blog',
+    href: routes.blog,
   },
   {
     title: 'Projects',
@@ -37,7 +35,6 @@ const Header = () => {
     >
       <div className="flex flex-wrap items-center justify-between max-w-2xl p-5 mx-auto">
         <nav className="flex items-center">
-          {/* <MobileNavigation className="ml-2" /> */}
           <RowNavigationLayout className="p-2" />
         </nav>
       </div>
@@ -48,36 +45,6 @@ const Header = () => {
 const RowNavigationLayout = ({ className }: { className?: string }) => {
   return (
     <NavigationLinks className={`flex items-center gap-x-4 ${className}`} />
-  )
-}
-
-const MobileNavigation = ({ className }: { className?: string }) => {
-  const [showMenu, setShowMenu] = useState(false)
-
-  return (
-    <div className={`relative sm:hidden ${className}`}>
-      <button
-        type="button"
-        onClick={() => setShowMenu((show) => !show)}
-        className="inline-flex items-center p-2 text-sm text-gray-400 rounded-lg md:hidden focus:outline-none ring-2 ring-gray-400/30 hover:bg-gray-700"
-        aria-label="toggle-mobile-navigation-menu"
-        aria-controls="navigation-menu"
-        aria-expanded={showMenu}
-      >
-        <GiHamburgerMenu />
-      </button>
-      {showMenu && (
-        <div
-          id="navigation-menu"
-          className="absolute right-0 p-4 rounded-lg w-44 bg-gray-700/90 top-9 ring-2 ring-gray-50/40"
-        >
-          <NavigationLinks
-            onLinkClick={() => setShowMenu(false)}
-            className="flex flex-col items-center gap-y-2"
-          />
-        </div>
-      )}
-    </div>
   )
 }
 
