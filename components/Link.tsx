cc2d5295bf9847b96a61659b1d0e575a63dc25be
gionny96 <gionathas@gmail.com>
2023-11-18 19:03:31 +0100

    feat: move `SocialLink` inside `Link`

--- components/Link.tsx
+++ components/Link.tsx
@@ -2,6 +2,7 @@ import NextLink from 'next/link'
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import { LinkProps } from 'lib/types'
+import { IconType } from 'react-icons/lib'
 
 const Link = ({ href, children, active, className, ...rest }: LinkProps) => {
   return (
@@ -22,6 +23,29 @@ export const LinkWithRightArrow = ({ href, children, ...rest }: LinkProps) => {
     <Link href={href} {...rest}>
       {children}
       <HiArrowNarrowRight className="w-4 h-4 ml-2" />
+    </Link>
+  )
+}
+
+export const SocialLink = ({
+  href,
+  icon: Icon,
+  title,
+  className = '',
+}: {
+  href: string
+  icon: IconType
+  title?: string
+  className?: string
+}) => {
+  return (
+    <Link className={className} target="_blank" rel="no-referrer" href={href}>
+      <Icon className="inline w-4 h-4" />
+      {title && (
+        <span className="ml-1 text-lg font-medium hover:underline hover:underline-offset-4">
+          {title}
+        </span>
+      )}
     </Link>
   )
 }
