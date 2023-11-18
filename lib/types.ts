c713b8575cd28a45feb1d903b0edb6153fa77d06
gionny96 <gionathas@gmail.com>
2023-11-18 18:28:09 +0100

    feat: implement link props

--- lib/types.ts
+++ lib/types.ts
@@ -2,6 +2,8 @@ import { PropsWithChildren } from 'react'
 import { LinkProps as NextLinkProps } from 'next/link'
 
 export type LinkProps = PropsWithChildren<NextLinkProps> & {
+  active?: boolean
+  className?: string
   target?: React.HTMLAttributeAnchorTarget
   rel?: React.HTMLAttributeReferrerPolicy
 }
